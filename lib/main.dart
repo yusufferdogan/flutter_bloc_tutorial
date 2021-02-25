59b6e89370f4b8d37c8e6d2da79230ad2211f52d
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-02-25 14:03:52 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_deneme/blocs/counter/bloc.dart';
+import 'package:bloc_deneme/blocs/counter_bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
